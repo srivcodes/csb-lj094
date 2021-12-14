7e20faccd0093cfe6e7be99e8a327b29a8f252bd
Code Sandbox <hello@codesandbox.io>
2021-12-14 18:07:14 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a09b42c0f9df7ad9375d830296c45478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b233203bba8b09dc04d1c2bead727678",
+    "url": "https://srivcodes.github.io/csb-lj094/index.html"
+  },
+  {
+    "revision": "07622c67549b211f9537",
+    "url": "https://srivcodes.github.io/csb-lj094/static/css/main.ce6e5570.chunk.css"
+  },
+  {
+    "revision": "c961cad235a4b4bbfebc",
+    "url": "https://srivcodes.github.io/csb-lj094/static/js/2.0bd27ae0.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://srivcodes.github.io/csb-lj094/static/js/2.0bd27ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07622c67549b211f9537",
+    "url": "https://srivcodes.github.io/csb-lj094/static/js/main.22b692bb.chunk.js"
+  },
+  {
+    "revision": "942e3c466a8fee883439",
+    "url": "https://srivcodes.github.io/csb-lj094/static/js/runtime-main.074a6992.js"
+  }
+]);
